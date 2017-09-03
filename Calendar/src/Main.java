f5139e3a2e6fb9c820b2259bb7f0d9705b582dcf
Greg <gregoryjjb@gmail.com>
2017-09-03 13:09:59 -0700

    Modified Calendar

--- Calendar/src/Main.java
+++ Calendar/src/Main.java
@@ -1,3 +1,7 @@
+import java.text.DateFormatSymbols;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Greg on 9/3/2017.
  */
@@ -5,6 +9,20 @@ public class Main
 {
 	public static void main(String[] args) {
 
-		System.out.println("Hello world");
+		GregorianCalendar gc = new GregorianCalendar();
+
+		Date foo = gc.getTime();
+		String month = monthName(gc.get(GregorianCalendar.MONTH));
+
+		System.out.println(month);
+	}
+
+	private static String monthName(int monthInt) {
+
+		if(monthInt > 11 || monthInt < 0) {
+			monthInt = 0;
+		}
+
+		return new DateFormatSymbols().getMonths()[monthInt];
 	}
 }
