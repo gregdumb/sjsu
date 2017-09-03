84d4d94214e5f4ee18fc1e2925d99f0c92080445
Greg <gregoryjjb@gmail.com>
2017-09-03 12:41:18 -0700

    Created calendar project

---
+++ Calendar/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Greg on 9/3/2017.
+ */
+public class Main
+{
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+	}
+}
---
+++ Calendar/src/UI.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Greg on 9/3/2017.
+ */
+public class UI
+{
+	public void output(String text) {
+		System.out.println(text);
+	}
+
+	public String prompt(String text) {
+
+		System.out.print(text);
+
+		return "";
+	}
+}
